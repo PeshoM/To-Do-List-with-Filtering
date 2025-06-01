1928a8116c90bff2f51ed6a530082274c98a777d
pesho <peshooomanov@gmail.com>
2025-06-01 10:43:00 +0300

    web-client: init

---
+++ web-client/src/components/FilteringButtons.tsx
@@ -0,0 +1,5 @@
+const FilteringButtons = () => {
+  return <div></div>;
+};
+
+export default FilteringButtons;
---
+++ web-client/src/components/TodoInput.tsx
@@ -0,0 +1,6 @@
+const TodoInput = () => {
+  return <div></div>;
+};
+
+export default TodoInput;
+
